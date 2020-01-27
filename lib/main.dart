2dcd0cc8072197e6d334ebb53c4e1b3981c6e68d
Mantab Tadayon <mt.tadayon@gmail.com>
2020-01-27 15:05:11 +0100

    Create routes for app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -16,6 +20,13 @@ class FlashChat extends StatelessWidget {
         ),
       ),
       home: WelcomeScreen(),
+      initialRoute: '/RegistrationScreen',
+      routes: {
+        '/WelcomeScreen': (context) => WelcomeScreen(),
+        '/RegistrationScreen': (context) => RegistrationScreen(),
+        '/LoginScreen': (context) => LoginScreen(),
+        '/ChatScreen': (context) => ChatScreen()
+      },
     );
   }
 }
